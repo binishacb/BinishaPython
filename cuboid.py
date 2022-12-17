f69c393ee9fc1cd7ab3eb117b947c722573dd632
binishacb <117909678+binishacb@users.noreply.github.com>
2022-12-18 00:09:57 +0530

    Add files via upload

---
+++ cuboid.py
@@ -0,0 +1,8 @@
+def area(l, b, h):
+    area = 2 * ((l * b) + (b * h) +( l * h))
+    print("area=",area)
+
+
+def perimeter(l, b, h):
+    perimeter = 4 * (l + b + h)
+    perimeter=("perimeter=",perimeter)
